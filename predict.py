8c1b91c3549e9a5cbe8624a33fcf2a9c97b12c89
qwertpi <roarser2004@gmail.com>
2018-09-27 16:02:44 +0100

    Fixed bugs and put resized image in /tmp as this is usualy RAM backed so should be quicker

--- predict.py
+++ predict.py
@@ -3,20 +3,31 @@ import numpy as np
 from keras.models import load_model
 from os import system as bash
 from time import time
-file="/some/file/path"
-command="convert "+file+' -resize 64x64 -gravity center -background "rgb(0,0,0)" -extent 64x64 '+file
+
+file="some/file/path"
+if file[-1]=="/":
+    file[:-1]
+
+command="convert "+file+' -resize 64x64 -gravity center -background "rgb(0,0,0)" -extent 64x64 '+"/tmp/"+file.split("/")[-1]
+file="/tmp/"+file.split("/")[-1]
 bash(command)
+
 img = image.load_img(file, target_size=None)
 img = np.array(img)
 img=np.array([img/255])
 model = load_model('model.h5')
+
 prediction=model.predict(img)
+
 if prediction.argmax()==0:
-    print("Cat with",prediction[0]*100+"%","certianty)
+    print("Cat with",str(prediction[0][0]*100)+"%","certianty")
 elif prediction.argmax()==1:
-    print("Dog with",prediction[1]*100+"%","certianty)
+    print("Dog with",str(prediction[0][0]*100)+"%","certianty")
 else:
     print("Well this is awkard. This isn't meant to happen. Please submit an issue on GitHub with the following information")
     print(prediction)
     print(command)
     print(file)
+    
+command="rm "+file
+bash(command)
