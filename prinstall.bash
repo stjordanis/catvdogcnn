eca820f976fb61554e18bb6d74473b2c9bf47f7c
qwertpi <qwertpi@users.noreply.github.com>
2018-10-05 21:11:02 +0100

    Fixed typos

--- prinstall.bash
+++ prinstall.bash
@@ -1,10 +1,10 @@
 echo 'Updating your packages'
 sudo apt-get update && sudo apt-get upgrade -y
-echo 'Installing Python, the Python Package Installer and software to downlaod code from github'
+echo 'Installing Python, the Python Package Installer and software to download code from GitHub'
 sudo apt-get install python3 python3-pip -y
 echo 'Downloading code from Github'
 git clone https://github.com/qwertpi/catvdogcnn.git catdogcnn
 cd catdogcnn
-echo 'Installing the requried python libaries'
+echo 'Installing the required python libraries'
 sudo pip3 install -r requirements.txt
 sudo apt-get install libhdf5-serial-dev
