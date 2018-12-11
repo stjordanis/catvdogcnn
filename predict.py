28d7dec4c70478c8677502338fd7eb3750ae1599
qwertpi <roarser2004@gmail.com>
2018-12-11 18:36:38 +0000

    Made tmp dir changable

--- predict.py
+++ predict.py
@@ -3,7 +3,8 @@ import numpy as np
 from keras.models import load_model
 from subprocess import check_output as bash
 
-if "tmpfs" in bash("df -T /tmp",shell=True).decode("utf-8"):
+tmp_dir='/tmp/'
+if "tmpfs" in bash("df -T "+tmp_dir,shell=True).decode("utf-8"):
     print("Your system supports writing to RAM so image loading should be fast :)")
 else:
     print("Your /tmp directory doesn't use RAM. This isn't an error it just means resizing the image will be a bit slower")
@@ -13,16 +14,14 @@ from os import system as bash
 file="/some/file/path"
 if file[-1]=="/":
     file[:-1]
-
-command="convert "+file+' -resize 64x64 -gravity center -background "rgb(0,0,0)" -extent 64x64 '+"/tmp/"+file.split("/")[-1]
-file="/tmp/"+file.split("/")[-1]
+model = load_model('model.h5')
+command="convert "+file+' -resize 64x64 -gravity center -background "rgb(0,0,0)" -extent 64x64 '+tmp_dir+file.split("/")[-1]
+file=tmp_dir+file.split("/")[-1]
 bash(command)
 
 img = image.load_img(file, target_size=None)
 img = np.array(img)
 img=np.array([img/255])
-model = load_model('model.h5')
-
 prediction=model.predict(img)
 
 if prediction.argmax()==0:
@@ -34,7 +33,8 @@ else:
     print(prediction)
     print(command)
     print(file)
-    print(bash("df -T /tmp",shell=True).decode("utf-8"))
-    
+    print(tmp_dir)
+    print(bash("df -T "+tmp_dir,shell=True).decode("utf-8"))
+
 command="rm "+file
 bash(command)
