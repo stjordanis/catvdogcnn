93f618accf7f9c86cd0f9e6e8fffc09e241e3c18
qwertpi <qwertpi@users.noreply.github.com>
2018-12-29 20:08:28 +0000

    Added comments

--- getdata.py
+++ getdata.py
@@ -8,9 +8,9 @@ import re
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-u", "--urls", required=True,
-	help="path to file containing image URLs")
+    help="path to file containing image URLs")
 ap.add_argument("-o", "--output", required=True,
-	help="path to output directory of images")
+    help="path to output directory of images")
 args = vars(ap.parse_args())
 
 # grab the list of URLs from the input file, then initialize the
@@ -20,19 +20,19 @@ total = 0
 
 # loop the URLs
 for url in rows:
-	try:
-	    #use requests to get the raw data of the url and open it in PIL
-	    #re.sub is beacuse removing the farm portion from static.flickr.com addresses makes them more likely to not 404
+    try:
+        #use requests to get the raw data of the url and open it in PIL
+        #re.sub is because removing the farm portion from static.flickr.com addresses makes them more likely to not 404
             im = Image.open(requests.get(re.sub('farm..','',url), stream=True).raw)
-	    #save the image with each image being concetivly named going 00000000.png then 00000001.png
+        #save the image with each image being concetivly named going 00000000.png then 00000001.png
             im.save(args["output"]+"{}.png".format(str(total).zfill(8)))
-	    #add 1 to the total variable
+        #add 1 to the total variable
             total += 1
-	    #every mutliple of 10 print the total
+        #every mutliple of 10 print the total
             if total%10==0:
                 print(total)
-	# handle if any exceptions are thrown during the download process
-	except:
-	    print("The image could not be found!")
+    # handle if any exceptions are thrown during the download process
+    except:
+        print("The image could not be found!")
 #print total at the end of running
 print(total)
