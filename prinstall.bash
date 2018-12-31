28571d1be94b020fad6f7508650ed180138b891b
qwertpi <qwertpi@users.noreply.github.com>
2018-12-31 17:37:52 +0000

    Added imagemagick and git to install command

--- prinstall.bash
+++ prinstall.bash
@@ -1,7 +1,7 @@
 echo 'Updating your packages'
 sudo apt-get update && sudo apt-get upgrade -y
-echo 'Installing Python, the Python Package Installer and software to download code from GitHub'
-sudo apt-get install python3 python3-pip -y
+echo 'Installing Python, the Python Package Installer, software to download code from GitHub and software to resize images'
+sudo apt-get install python3 python3-pip git imagemagick -y
 echo 'Downloading code from Github'
 git clone https://github.com/qwertpi/catvdogcnn.git catdogcnn
 cd catdogcnn
